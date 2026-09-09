fdd766475f4f3b57511d6c99513b284c04dfef93
arshtyi <arshtyi@foxmail.com>
2026-09-10 07:56:29 +0800

    lab: 2-3

---
+++ lab/2/src/3.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+
+
+def single_neuron_model(features, labels, weights, bias):
+    z = np.dot(features, weights) + bias
+    probabilities = 1 / (1 + np.exp(-z))
+    mse = np.mean((probabilities - labels) ** 2)
+    return np.round(probabilities, 4).tolist(), round(float(mse), 4)
+
+
+if __name__ == "__main__":
+    features = np.array(eval(input()))
+    labels = np.array(eval(input()))
+    weights = np.array(eval(input()))
+    bias = float(input())
+    print(single_neuron_model(features, labels, weights, bias))
