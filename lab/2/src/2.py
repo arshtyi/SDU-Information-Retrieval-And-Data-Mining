d35ce959812f5df3ba45673af629a9d1063aacab
arshtyi <arshtyi@foxmail.com>
2026-09-09 23:46:00 +0800

    lab: 2-2

---
+++ lab/2/src/2.py
@@ -0,0 +1,13 @@
+import numpy as np
+import math
+
+
+def softmax(scores: list[float]) -> list[float]:
+    x = np.array(scores)
+    exp_x = np.exp(x - np.max(x))
+    return np.round(exp_x / np.sum(exp_x), 4).tolist()
+
+
+if __name__ == "__main__":
+    scores = np.array(eval(input()))
+    print(softmax(scores))
