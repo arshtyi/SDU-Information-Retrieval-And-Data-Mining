45b1cc0d8a50802c3dbf29f47408d3311f74b83a
arshtyi <arshtyi@foxmail.com>
2026-09-10 08:13:42 +0800

    lab: 2-6

---
+++ lab/2/src/6.py
@@ -0,0 +1,12 @@
+import numpy as np
+from typing import Union
+
+
+def leaky_relu(z, alpha=0.01):
+    return np.maximum(alpha * z, z)
+
+
+if __name__ == "__main__":
+    z = eval(input())
+    alpha = eval(input())
+    print(leaky_relu(z, alpha))
